5ae9c62b69b00407c51b1106a76bc67975ff3937
teddy <tedocastel@gmail.com>
2024-08-30 19:58:39 -0600

    Primer parte del proyecto

---
+++ src/main/java/com/mycompany/los_panchos/Los_Panchos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.los_panchos;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Los_Panchos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
